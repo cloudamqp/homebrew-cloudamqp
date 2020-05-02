b633c9415edf2ceeafba63aa9d518e1e8385995e
Carl Hörberg <carl.hoerberg@gmail.com>
2020-05-02 14:44:51 +0200

    put head after url/sha

--- Formula/amqpcat.rb
+++ Formula/amqpcat.rb
@@ -1,9 +1,9 @@
 class Amqpcat < Formula
   desc "CLI tool for publishing to and consuming from AMQP servers"
   homepage "https://github.com/cloudamqp/amqpcat"
-  head "https://github.com/cloudamqp/amqpcat.git"
   url "https://github.com/cloudamqp/amqpcat/archive/v0.2.0.tar.gz"
   sha256 "2e740017e63bf34f4d966b27fe4e52caabe660f1169b100496c225cd19b21c65"
+  head "https://github.com/cloudamqp/amqpcat.git"
 
   depends_on "crystal" => :build
 
