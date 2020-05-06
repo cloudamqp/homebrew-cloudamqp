e66a54cca394a19cec13995ca1bf267ceec2b916
Carl Hörberg <carl.hoerberg@gmail.com>
2020-05-06 10:17:57 +0200

    bump amqpcat to 0.2.1

--- Formula/amqpcat.rb
+++ Formula/amqpcat.rb
@@ -1,8 +1,8 @@
 class Amqpcat < Formula
   desc "CLI tool for publishing to and consuming from AMQP servers"
   homepage "https://github.com/cloudamqp/amqpcat"
-  url "https://github.com/cloudamqp/amqpcat/archive/v0.2.0.tar.gz"
-  sha256 "2e740017e63bf34f4d966b27fe4e52caabe660f1169b100496c225cd19b21c65"
+  url "https://github.com/cloudamqp/amqpcat/archive/v0.2.1.tar.gz"
+  sha256 "910502076018521c0df788a8100e72fa0852d8352e64ccbc09d647c5ab6911d3"
   head "https://github.com/cloudamqp/amqpcat.git"
 
   depends_on "crystal" => :build
