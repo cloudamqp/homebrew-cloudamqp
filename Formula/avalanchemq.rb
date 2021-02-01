005dfb147e6368784c2a0e8eb41ae6633f06c3a7
Patrik Ragnarsson <patrik@starkast.net>
2021-02-01 17:44:42 +0100

    AvalancheMQ v1.0.0-alpha.25.tar.gz

--- Formula/avalanchemq.rb
+++ Formula/avalanchemq.rb
@@ -1,8 +1,8 @@
 class Avalanchemq < Formula
   desc "AMQP 0-9-1 server"
   homepage "https://github.com/cloudamqp/avalanchemq"
-  url "https://github.com/cloudamqp/avalanchemq/archive/v1.0.0-alpha.23.tar.gz"
-  sha256 "a533d09b64c2b1e8e262a034736547371a23a7e439b35b96d9e6d1bb7331991c"
+  url "https://github.com/cloudamqp/avalanchemq/archive/v1.0.0-alpha.25.tar.gz"
+  sha256 "ab23acd6362d62d082f55e7b6347598fc2792166f516eb7c00fad5876a4b6ba3"
   head "https://github.com/cloudamqp/avalanchemq.git"
 
   depends_on "crystal" => :build
