d1bf93cb4d126e3d1d774309ee3b9662789054be
Carl Hörberg <carl@84codes.com>
2021-11-04 14:15:53 +0100

    amqpcat 0.2.4

--- Formula/amqpcat.rb
+++ Formula/amqpcat.rb
@@ -1,17 +1,14 @@
 class Amqpcat < Formula
   desc "CLI tool for publishing to and consuming from AMQP servers"
   homepage "https://github.com/cloudamqp/amqpcat"
-  url "https://github.com/cloudamqp/amqpcat/archive/v0.2.3.tar.gz"
-  sha256 "f2179ee77e12755852ce7e9dd0c609acebd55fb9a5acb718a2e2ede9df68be4e"
+  url "https://github.com/cloudamqp/amqpcat/archive/v0.2.4.tar.gz"
+  sha256 "4dccbd37e4cee1bc0119748921c9bbce86137bc6ba870b1861823ce76a09e3eb"
   head "https://github.com/cloudamqp/amqpcat.git"
 
   depends_on "crystal" => :build
 
   def install
-    system "shards", "build",
-                     "--release",
-                     "--production",
-                     "--no-debug"
+    system "shards", "build", "--release", "--production", "--no-debug"
     bin.install "bin/amqpcat"
   end
 
