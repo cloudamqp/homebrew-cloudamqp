d5aeeed0fde0efc1ef8964602a092506163770b5
Magnus Landerblom <magnus@landerblom.se>
2022-05-24 15:47:47 +0200

    Build LavinMQ correctly

--- Formula/lavinmq.rb
+++ Formula/lavinmq.rb
@@ -1,6 +1,6 @@
 class Lavinmq < Formula
   desc "Fast and efficient AMQP 0-9-1 server"
-  homepage "https://github.com/cloudamqp/lavinmq"
+  homepage "https://www.lavinmq.com"
   url "https://github.com/cloudamqp/lavinmq/archive/v1.0.0-beta.1.tar.gz"
   sha256 "cf28eb19afb30d20f0b3b5541d3a5bd3a59ac832a37b678c4364530bcb9e12ea"
   head "https://github.com/cloudamqp/lavinmq.git"
@@ -8,9 +8,7 @@ class Lavinmq < Formula
   depends_on "crystal" => :build
 
   def install
-    system "shards", "build",
-                     "--release",
-                     "--production"
+    system "make" "all"
     bin.install "bin/lavinmq"
     bin.install "bin/lavinmqctl"
     bin.install "bin/lavinmqperf"
