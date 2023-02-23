2363c718648c143aec6fb98ee63d06cd137403c8
Oskar Gustafsson <oskar.isidor.gustafsson@gmail.com>
2023-02-23 13:22:56 +0100

    Bump sparoid to 1.1.11

--- Formula/sparoid.rb
+++ Formula/sparoid.rb
@@ -1,8 +1,8 @@
 class Sparoid < Formula
   desc "SPA client"
   homepage "https://github.com/84codes/sparoid"
-  url "https://github.com/84codes/sparoid/archive/v1.0.1.tar.gz"
-  sha256 "d936308f1a06638f7713694aac96dee8d403ceeecd03981f403cfd40933c3f5d"
+  url "https://github.com/84codes/sparoid/archive/v1.1.11.tar.gz"
+  sha256 "29606ef48e5600a521b4cb733489725383959acb78959290387682f936f5d833"
   head "https://github.com/84codes/sparoid.git"
 
   depends_on "crystal" => :build
