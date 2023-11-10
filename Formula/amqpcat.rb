b38f72fede5905bf6f0c830d1d71a104a14aaab1
Johan Rhodin <johan@84codes.com>
2023-11-10 15:49:09 -0600

    Change to using https://github.com/cloudamqp/amqpcat/archive/refs/tags/v0.2.5.tar.gz

--- Formula/amqpcat.rb
+++ Formula/amqpcat.rb
@@ -1,7 +1,7 @@
 class Amqpcat < Formula
   desc "CLI tool for publishing to and consuming from AMQP servers"
   homepage "https://github.com/cloudamqp/amqpcat"
-  url "https://github.com/cloudamqp/amqpcat/archive/v0.2.5.tar.gz"
+  url "https://github.com/cloudamqp/amqpcat/archive/refs/tags/v0.2.5.tar.gz"
   sha256 "05917d52a06bdd7a45e85f620117a93f9caa0747f2468a65862d53b8b7ca6999"
   head "https://github.com/cloudamqp/amqpcat.git"
 
