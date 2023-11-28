b8ea204247386451195aa332af9021e5ce41504a
Christina <christina@84codes.com>
2023-11-28 15:38:49 +0100

    use refs/tags for lavinmq formula

--- Formula/lavinmq.rb
+++ Formula/lavinmq.rb
@@ -1,7 +1,7 @@
 class Lavinmq < Formula
   desc "Fast and efficient AMQP 0-9-1 server"
   homepage "https://www.lavinmq.com"
-  url "https://github.com/cloudamqp/lavinmq/archive/v1.2.5.tar.gz"
+  url "https://github.com/cloudamqp/lavinmq/archive/refs/tags/v1.2.5.tar.gz"
   sha256 "6529b81f1601840cb370d9cc54d70f422cf080bbe280c3a3738e276c0c6bd405"
   head "https://github.com/cloudamqp/lavinmq.git"
 
