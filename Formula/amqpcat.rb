a8965ce84785c4663bc88351307709c4bedda746
Lucas Polesello <lucas.c4d@gmail.com>
2026-02-26 14:29:32 -0300

    use `main` on HEAD and use openssl@3 on depends_on

--- Formula/amqpcat.rb
+++ Formula/amqpcat.rb
@@ -3,10 +3,10 @@ class Amqpcat < Formula
   homepage "https://github.com/cloudamqp/amqpcat"
   url "https://github.com/cloudamqp/amqpcat/archive/refs/tags/v1.1.0.tar.gz"
   sha256 "d08973a92748601188c90caa4ec31b8f8f8efa2a6c202661bff8c6a527b3a861"
-  head "https://github.com/cloudamqp/amqpcat.git"
+  head "https://github.com/cloudamqp/amqpcat.git", branch: "main"
 
   depends_on "crystal" => :build
-  depends_on "openssl" => :build
+  depends_on "openssl@3"
 
   def install
     system "shards", "build", "--release", "--production", "--no-debug"
