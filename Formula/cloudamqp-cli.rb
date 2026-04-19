7cccf2942a3c1c90b4b25ff1e85618af8927f6ca
markus812498 <markus@84codes.com>
2026-04-20 09:33:25 +1200

    new version of cloudamqp-cli

--- Formula/cloudamqp-cli.rb
+++ Formula/cloudamqp-cli.rb
@@ -1,8 +1,8 @@
 class CloudamqpCli < Formula
   desc "CLI tool for managing CloudAMQP instances"
   homepage "https://github.com/cloudamqp/cli"
-  url "https://github.com/cloudamqp/cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "1464d530d0ade8d63e77961cd63832d4e7f2cff9d9361a9a7b853bb9189f1b6f"
+  url "https://github.com/cloudamqp/cli/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "c10a907c47d5d07d8c7c27c6aef1ecdd41f487e76a25ee87ab329893fc2a6fcf"
   license "MIT"
 
   depends_on "go" => :build
